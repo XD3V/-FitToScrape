a0fb4b23077de0ee6d9375874087d2caa56a9e3d
Xavier Davis <xdavis1@icloud.com>
2019-12-03 14:14:38 -0800

    make it better

--- server.js
+++ server.js
@@ -36,21 +36,20 @@ mongoose.connect(MONGODB_URI);
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
     // First, we grab the body of the html with axios
-    axios.get("https://www.nytimes.com/").then(function(response) {
+    axios.get("https://www.nytimes.com/column/sciencetake").then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
   
       // Now, we grab every h2 within an article tag, and do the following:
-      $("article h2").each(function(i, element) {
+      $("li").each(function(i, element) {
         // Save an empty result object
         var result = {};
   
         // Add the text and href of every link, and save them as properties of the result object
         result.title = $(this)
-          .children("a")
           .text();
         result.link = $(this)
-          .children("a")
+            .find("a")
           .attr("href");
   
         // Create a new Article using the `result` object built from scraping
